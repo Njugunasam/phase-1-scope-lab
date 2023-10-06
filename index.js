736ab773f5189ce75bbeb4a68a61cdcd4c3ebfd8
Njugunasam <sammynjuguna5826@gmail.com>
2023-10-06 22:10:14 +0300

    fixing errors

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(newFavorite) {
+    bestCustomer = newFavorite;
+}
+function changeLeastFavoriteCustomer(newLeastFavorite) {
+    leastFavoriteCustomer = newLeastFavorite;
+}
